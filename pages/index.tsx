f2c2163a196e2ff063d951b7a31d2cb6287a440c
monkuma <kumamoto.jobs@gmail.com>
2023-03-22 15:49:10 +0900

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-2xl text-green-500">Netflix Clone!</h1>
+    </>
+  );
+};
+
+export default Home;
